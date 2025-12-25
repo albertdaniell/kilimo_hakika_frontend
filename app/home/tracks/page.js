b9b1286eda70c0f7fa880ae69db5a52d5993f4a9
albertdaniell <albertagoya@gmail.com>
2025-12-25 11:25:49 +0300

    adding tracks selection

--- app/home/tracks/page.js
+++ app/home/tracks/page.js
@@ -50,7 +50,7 @@ const tracks = [
 
 export default function TracksPage() {
   return (
-    <div className="p-8 bg-gray-100 min-h-screen">
+    <div className="px-8 min-h-screen">
       <h1 className="text-3xl font-bold mb-6 text-gray-900">Available Tracks</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
